4466c1193254851d1152c80df260114f226c1578
Cristian Hernandez <ing.cristianh@gmail.com>
2020-03-06 18:53:42 -0500

    Appstack

--- src/navigations/AppContainer.js
+++ src/navigations/AppContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import AuthStack from './AppStack';
 import AppStack from './AppStack';
 
 const AppContainer: () => React = () => {
